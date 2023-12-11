b59dec3884e29d4e6461e96de60e7c9f1eea82e8
Sat Naing Tun <satnaingtun.snt@gmail.com>
2023-12-11 10:10:11 +0630

    first commit

---
+++ SNT_MMUnicode_Word2010/Ribbon1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SNT_MMUnicode_Word2010
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SNT_MMUnicode = this.Factory.CreateRibbonTab();
+            this.Convert = this.Factory.CreateRibbonGroup();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.ZGToUni = this.Factory.CreateRibbonButton();
+            this.U_Zaw = this.Factory.CreateRibbonButton();
+            this.btnSavePdf = this.Factory.CreateRibbonButton();
+            this.btnXPS = this.Factory.CreateRibbonButton();
+            this.SNT_MMUnicode.SuspendLayout();
+            this.Convert.SuspendLayout();
+            this.group1.SuspendLayout();
+            // 
+            // SNT_MMUnicode
+            // 
+            this.SNT_MMUnicode.Groups.Add(this.Convert);
+            this.SNT_MMUnicode.Groups.Add(this.group1);
+            this.SNT_MMUnicode.Label = "SNT_MMUnicode";
+            this.SNT_MMUnicode.Name = "SNT_MMUnicode";
+            // 
+            // Convert
+            // 
+            this.Convert.Items.Add(this.ZGToUni);
+            this.Convert.Items.Add(this.U_Zaw);
+            this.Convert.Label = "Convert";
+            this.Convert.Name = "Convert";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnSavePdf);
+            this.group1.Items.Add(this.btnXPS);
+            this.group1.Label = "Save";
+            this.group1.Name = "group1";
+            // 
+            // ZGToUni
+            // 
+            this.ZGToUni.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.ZGToUni.Image = global::SNT_MMUnicode_Word2010.Properties.Resources.Unicode;
+            this.ZGToUni.Label = "ZGToUni";
+            this.ZGToUni.Name = "ZGToUni";
+            this.ZGToUni.ShowImage = true;
+            this.ZGToUni.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ZGToUni_Click);
+            // 
+            // U_Zaw
+            // 
+            this.U_Zaw.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.U_Zaw.Image = global::SNT_MMUnicode_Word2010.Properties.Resources.Zg;
+            this.U_Zaw.Label = "Uni To Zaw";
+            this.U_Zaw.Name = "U_Zaw";
+            this.U_Zaw.ShowImage = true;
+            this.U_Zaw.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.U_Zaw_Click);
+            // 
+            // btnSavePdf
+            // 
+            this.btnSavePdf.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnSavePdf.Image = global::SNT_MMUnicode_Word2010.Properties.Resources.pdf1;
+            this.btnSavePdf.Label = "Save as Pdf";
+            this.btnSavePdf.Name = "btnSavePdf";
+            this.btnSavePdf.ShowImage = true;
+            this.btnSavePdf.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSavePdf_Click);
+            // 
+            // btnXPS
+            // 
+            this.btnXPS.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnXPS.Image = global::SNT_MMUnicode_Word2010.Properties.Resources.XPS;
+            this.btnXPS.Label = "Save as XPS";
+            this.btnXPS.Name = "btnXPS";
+            this.btnXPS.ShowImage = true;
+            this.btnXPS.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnXPS_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.SNT_MMUnicode);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.SNT_MMUnicode.ResumeLayout(false);
+            this.SNT_MMUnicode.PerformLayout();
+            this.Convert.ResumeLayout(false);
+            this.Convert.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab SNT_MMUnicode;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Convert;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ZGToUni;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnSavePdf;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnXPS;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton U_Zaw;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ SNT_MMUnicode_Word2010/Ribbon1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Tools.Word;
+using Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SNT_MMUnicode_Word2010
+{
+    public partial class Ribbon1
+    {
+        string output, input;
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void ZGToUni_Click(object sender, RibbonControlEventArgs e)
+        {
+
+            /*
+            if (Globals.ThisAddIn.Application.Documents.Count > 0)
+            {
+                Microsoft.Office.Interop.Word.Document nativeDocument =
+                Globals.ThisAddIn.Application.ActiveDocument;
+                Microsoft.Office.Tools.Word.Document vstoDocument =
+                Globals.Factory.GetVstoObject(nativeDocument);
+            }
+             * */
+            Word.Range currentRange = Globals.ThisAddIn.Application.Selection.Range;
+            input = currentRange.Text;
+            /*
+             foreach (char s in input) {
+               output+=Rabbit.Zg2Uni(s.ToString());
+                
+            
+             }*/
+
+            output = Rabbit.Zg2Uni(input);
+            currentRange.Text = output;
+            currentRange.Font.Name = "Pyidaungsu"; 
+        }
+        private void U_Zaw_Click(object sender, RibbonControlEventArgs e)
+        {
+
+            /*
+            if (Globals.ThisAddIn.Application.Documents.Count > 0)
+            {
+                Microsoft.Office.Interop.Word.Document nativeDocument =
+                Globals.ThisAddIn.Application.ActiveDocument;
+                Microsoft.Office.Tools.Word.Document vstoDocument =
+                Globals.Factory.GetVstoObject(nativeDocument);
+            }
+             * */
+            Word.Range currentRange = Globals.ThisAddIn.Application.Selection.Range;
+            input = currentRange.Text;
+            /*
+             foreach (char s in input) {
+               output+=Rabbit.Zg2Uni(s.ToString());
+                
+            
+             }*/
+
+            output = Rabbit.Uni2Zg(input);
+            currentRange.Text = output;
+            currentRange.Font.Name = "Zawgyi-One";
+        }
+
+        private void btnSavePdf_Click(object sender, RibbonControlEventArgs e)
+        {
+            //string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //string fileName = "QuickExport.pdf";
+            // SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "PDF Files|*.pdf";
+            saveFileDialog1.Title = "Save a PDF File";
+            saveFileDialog1.InitialDirectory = @"D:\";
+            saveFileDialog1.ShowDialog();
+            var file = new FileInfo(saveFileDialog1.FileName);
+            Globals.ThisAddIn.Application.ActiveDocument.ExportAsFixedFormat(
+                Path.Combine(file.DirectoryName, saveFileDialog1.FileName),
+                Word.WdExportFormat.wdExportFormatPDF,
+                OpenAfterExport: true);
+        }
+
+        private void btnXPS_Click(object sender, RibbonControlEventArgs e)
+        {
+            //string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //string fileName = "QuickExport.xps";
+            saveFileDialog1.Filter = "XPS Files|*.XPS";
+            saveFileDialog1.Title = "Save a XPS File";
+            saveFileDialog1.InitialDirectory = @"D:\";
+            saveFileDialog1.ShowDialog();
+            var file = new FileInfo(saveFileDialog1.FileName);
+            Globals.ThisAddIn.Application.ActiveDocument.ExportAsFixedFormat(
+                Path.Combine(file.DirectoryName, saveFileDialog1.FileName),
+                Word.WdExportFormat.wdExportFormatXPS,
+                OpenAfterExport: true);
+
+        }
+    }
+}
